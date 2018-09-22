0b893188959247077b251d80d972e47f3430392a
Dawn Tepper <dawn.tepper@associates.hq.dhs.gov>
2018-09-22 14:34:35 -0400

    Create new Spring Application

---
+++ btg/src/main/java/myApi/MyControllerTest.java
@@ -0,0 +1,28 @@
+package mine;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MyControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getMine() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from dtepdc test!")));
+    }
+}
